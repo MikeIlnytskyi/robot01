056b06da970752c347cda73ec614bcd655f5bc18
Mike <mike.ilnytskyi@gmail.com>
2024-12-22 14:49:24 +0200

    init commit

---
+++ src/test/java/com/example/robots0_1/Robots01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.robots0_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Robots01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
